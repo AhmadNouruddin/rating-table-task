cc3b16312d72388656bb0ff6512ed79da92fc88b
AhmadNouruddin <61498903+AhmadNouruddin@users.noreply.github.com>
2024-02-23 09:27:03 +0300

    Update rating_script.py
    Refactor script, make it more functional and independent by making a function for each task

--- rating_script.py
+++ rating_script.py
@@ -1,57 +1,68 @@
 import pandas as pd
 from jinja2 import Environment, FileSystemLoader
 
-df = pd.read_csv('./Rating_Table.csv')
 
-# Cleaning prices from string and leaving only numbers and NaN.
-for column in df.columns[df.columns.get_loc('Receiving call cost per min'):]:
-    df[column] = pd.to_numeric(df[column], errors='coerce')
+def clean_data():
+    df = pd.read_csv('./Rating_Table.csv')
 
-env = Environment(loader=FileSystemLoader('./'))
-template = env.get_template('RATING_XML.xml')
+    # Cleaning prices from string and leaving only numbers
+    for column in df.columns[df.columns.get_loc('Receiving call cost per min'):]:
+        df[column] = pd.to_numeric(
+            df[column], errors='coerce', downcast='float')
+
+    # dealing with NaNs
+    for index, row in df.iterrows():
+        if not pd.isna(row['Company code']) and not pd.isna(row['Company number']):
+            # fill NaN with 0
+            df.fillna(0, inplace=True)
+        else:
+            # if Company code & Company number are NaN drop entire row
+            new_df = df.drop(index)
+    return new_df
 
 
-def write_content_to_xml(index, row):
-    filename = f"RATING_XML_{row['Company code']}.xml"
-    content = template.render({
+def load_template_and_get_variables(row, round_decimal):
+    env = Environment(loader=FileSystemLoader('./'))
+    template = env.get_template('RATING_XML.xml')
+    content = {
         'region': row['Region'],
         'country': row['Country'],
         'company_name': row['Company name'],
         'company_number': row['Company number'],
         'company_code': row['Company code'],
         'company_features': row['Company features'],
-        'receiving_call_cost_per_min': round(float(row['Receiving call cost per min']), 2),
-        'local_call_cost_per_min': round(float(row['Local call cost per min']), 2),
-        'calling_to_me_cost_per_min': round(float(row['Calling to ME cost per min']), 2),
-        'calling_to_other_destinations_cost_per_min': round(float(row['Calling to other destinations cost per min']), 2),
-        'sms_mo_cost_per_sms': round(float(row['SMS  MO cost per sms']), 2),
-        'sms_mt_cost_per_sms': round(float(row['SMS MT cost per sms']), 2),
-        'data_cost_per_mb': round(float(row['Data cost per MB']), 2),
+        'receiving_call_cost_per_min': round(row['Receiving call cost per min'], round_decimal),
+        'local_call_cost_per_min': round(row['Local call cost per min'], round_decimal),
+        'calling_to_me_cost_per_min': round(row['Calling to ME cost per min'], round_decimal),
+        'calling_to_other_destinations_cost_per_min': round(row['Calling to other destinations cost per min'],
+                                                            round_decimal),
+        'sms_mo_cost_per_sms': round(row['SMS  MO cost per sms'], round_decimal),
+        'sms_mt_cost_per_sms': round(row['SMS MT cost per sms'], round_decimal),
+        'data_cost_per_mb': round(row['Data cost per MB'], round_decimal),
         'hpmn': 'HPMN',
         'effective_date': '2024',
         'submission_dt': 'SubmissionDT',
         'iot_identifier': 'IOTIdentifier',
         'correction_sequence': 'CorrectionSequence',
-        'iot_currency': 'USD'})
-    # creating xml file.
+        'iot_currency': 'USD'}
+    return template, content
+
+
+def write_content_to_xml(row):
+    filename = f"RATING_XML_{row['Company code']}.xml"
+    template, content = load_template_and_get_variables(
+        row=row, round_decimal=2)
+    content = template.render(content)
+    # creating xml file
     with open(filename, mode='w', encoding='utf-8') as new_xml:
         new_xml.write(content)
-        print(f'{index}... wrote {filename}')
+        print(f'... wrote {filename}')
 
 
-# Iterating over rows.
-for index, row in df.iterrows():
-    # checking for rows with no NaN values in df.
-    if not row.isnull().values.any():
-        write_content_to_xml(index, row)
-    # rows with no NaN values in df
-    else:
-        # checking for rows with NaN values but Company code & Company number exist.
-        if not pd.isna(df.at[index, 'Company code']) and not pd.isna(df.at[index, 'Company number']):
-            # fill NaN with 0
-            row.fillna(0, inplace=True)
-            write_content_to_xml(index, row)
-        else:
-            # Company code & Company number are NaN drop entire row.
-            row.dropna(inplace=True)
-            print(f'{index}... It\'s not a valid row.')
+def rating_app():
+    for index, row in clean_data().iterrows():
+        write_content_to_xml(row)
+
+
+if __name__ == '__main__':
+    rating_app()
